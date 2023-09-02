3db3a7f5106b504fa0efa646337d389cb97d03c6
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-09-02 09:57:07 +0200

    [error fix] move Bottombar into the body of app/root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
             <RightSidebar />
           </main>
           {children}
+          <Bottombar />
         </body>
-        <Bottombar />
       </html>
     </ClerkProvider>
   );
