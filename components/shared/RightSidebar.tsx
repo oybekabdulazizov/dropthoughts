d16786dbbedceae2a62334d57883e17807a99d06
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-09-19 11:39:35 +0200

    comment out suggested communities, and communitytabs

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,7 +3,8 @@ export default function RightSidebar() {
     <section className='custom-scrollbar rightsidebar'>
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>
-          Suggested Communities
+          {/* Suggested Communities */}
+          Suggested Threads
         </h3>
       </div>
       <div className='flex flex-1 flex-col justify-start'>
