83028f6733a3bb9ed98178f677d0d37b0c16992a
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-09-28 11:28:52 +0200

    show suggested users, remove suggested threads section

---
+++ components/cards/SmallUserCard.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+interface Props {
+  idUser_clerk: string;
+  name: string;
+  username: string;
+  image: string;
+  threads: any[];
+}
+
+export default function SmallUserCard({
+  idUser_clerk,
+  name,
+  username,
+  image,
+  threads,
+}: Props) {
+  return (
+    <div className='flex flex-col justify-start w-full bg-dark-2 rounded-lg mt-6'>
+      <section className='flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <Link href={`/profile/${idUser_clerk}`}>
+            <Image
+              src={image}
+              alt={`Profile image of ${name}`}
+              width={50}
+              height={50}
+              className='rounded-full object-contain shadow-2xl'
+            />
+          </Link>
+          <Link href={`/profile/${idUser_clerk}`} className='flex flex-col'>
+            <h1 className='text-left text-base1-semibold text-light-1'>
+              {name}
+            </h1>
+            <p className='mt-1 text-small-semibold text-gray-1'>@{username}</p>
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+}
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,14 +1,31 @@
-export default function RightSidebar() {
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import SmallUserCard from '../cards/SmallUserCard';
+import { currentUser } from '@clerk/nextjs';
+
+export default async function RightSidebar() {
+  const currentUser_clerk = await currentUser();
+
+  const currentUser_db = await fetchUser(currentUser_clerk?.id || '');
+
+  const users = await fetchUsers();
+  const orderedUsers = users
+    .filter((user) => !user._id.equals(currentUser_db._id))
+    .sort((a, b) => b.threads.length - a.threads.length);
+
   return (
     <section className='custom-scrollbar rightsidebar'>
       <div className='flex flex-1 flex-col justify-start'>
-        <h3 className='text-heading4-medium text-light-1'>
-          {/* Suggested Communities */}
-          Suggested Threads
-        </h3>
-      </div>
-      <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+        {orderedUsers.map((user) => (
+          <SmallUserCard
+            key={user._id}
+            idUser_clerk={user.idUser_clerk}
+            name={user.name}
+            username={user.username}
+            image={user.image}
+            threads={user.threads}
+          />
+        ))}
       </div>
     </section>
   );
