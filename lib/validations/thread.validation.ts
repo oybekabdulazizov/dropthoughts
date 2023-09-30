aba7424fa6640754882ab1dcb2bcfcc846e6f5cf
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-09-30 17:14:03 +0200

    fix the thread validation

--- lib/validations/thread.validation.ts
+++ lib/validations/thread.validation.ts
@@ -3,17 +3,17 @@ import * as z from 'zod';
 export const ThreadValidation = z.object({
   thread: z
     .string()
-    .nonempty()
     .min(3, 'A thread must contain minumum 3 characters.')
-    .max(500, 'A thread cannot exceed 500 characters'),
+    .max(500, 'A thread cannot exceed 500 characters')
+    .nonempty(),
   author: z.string(),
 });
 
 export const CommentValidation = z.object({
   thread: z
     .string()
-    .nonempty()
     .min(3, 'A thread must contain minumum 3 characters.')
-    .max(500, 'A thread cannot exceed 500 characters'),
+    .max(500, 'A thread cannot exceed 500 characters')
+    .nonempty(),
   author: z.string(),
 });
